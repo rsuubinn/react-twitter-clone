3371c47a2d8191c96109a45be4504d790e752688
rsuubinn <rsuubinn@gmail.com>
2023-03-10 23:24:41 +0900

    Test: 로그인 폼 테스트

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import firebase, { authService } from "../firebase";
 import AppRouter from "./Router";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true); // 초기에는 user가 null 값이지만 login 하면 user가 존재함
+      } else setIsLoggedIn(false);
+      setInit(true); // 초기화 완료
+    });
+  }, []);
+  return (
+    <>{init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}</>
+  );
 }
 
 export default App;
