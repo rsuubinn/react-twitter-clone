c6d732d23a2144e0ea014846fc7f106e42cb327c
rsuubinn <rsuubinn@gmail.com>
2023-03-13 13:33:22 +0900

    Feat: 트윗 입력 폼, 데이터베이스 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [tweet, setTweet] = useState("");
+  const onChange = (event) => {
+    setTweet(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+        />
+        <input type="submit" value="tweet" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
