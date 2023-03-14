f0b4adb48e5db175c5faf89db3cfe7f64b39e484
rsuubinn <rsuubinn@gmail.com>
2023-03-14 22:51:04 +0900

    Feat : 트윗 생성하기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "../firebase";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
   const onChange = (event) => {
     setTweet(event.target.value);
   };
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    await dbService.collection("tweets").add({
+      tweet,
+      createdAt: Date.now(),
+    });
+    setTweet("");
   };
+
+  const getTweets = async () => {
+    const dbTweets = await dbService.collection("tweets").get();
+    dbTweets.forEach((document) => {
+      const newTweet = {
+        ...document.data(),
+        id: document.id,
+      };
+      setTweets((prev) => [newTweet, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getTweets();
+  }, []);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -19,6 +40,11 @@ const Home = () => {
         />
         <input type="submit" value="tweet" />
       </form>
+      <div>
+        {tweets.map((tweet) => (
+          <h4 key={tweet.id}>{tweet.tweet}</h4>
+        ))}
+      </div>
     </div>
   );
 };
