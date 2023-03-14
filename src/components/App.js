960d03e214826a1a2e028364cb0892dc2318e1e0
rsuubinn <rsuubinn@gmail.com>
2023-03-15 02:42:00 +0900

    Feat : 트윗 수정하기

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,22 @@
 import React, { useEffect, useState } from "react";
-import firebase, { authService } from "../firebase";
+import { authService } from "../firebase";
 import AppRouter from "./Router";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [init, setInit] = useState(false);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true); // 초기에는 user가 null 값이지만 login 하면 user가 존재함
         setUserObj(user);
-      } else setIsLoggedIn(false);
+      }
       setInit(true); // 초기화 완료
     });
   }, []);
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
       ) : (
         "Initializing..."
       )}
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,19 +1,46 @@
+import { useState } from "react";
 import { dbService } from "../firebase";
 
 const Tweet = ({ tweet, isOwner }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTweet, setNewTweet] = useState(tweet.text);
   const onDeleteClick = async () => {
     const ok = window.confirm("트윗을 삭제하시겠습니까?");
     if (ok) {
       await dbService.doc(`tweets/${tweet.id}`).delete();
     }
   };
+  const toggleIsEditing = () => setIsEditing((prev) => !prev);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewTweet(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.doc(`/tweets/${tweet.id}`).update({ text: newTweet });
+    setIsEditing(false);
+  };
   return (
     <div>
-      <h4>{tweet.text}</h4>
-      {isOwner && (
+      {isEditing ? (
         <>
-          <button onClick={onDeleteClick}>삭제하기</button>
-          <button>수정하기</button>
+          <form onSubmit={onSubmit}>
+            <input type="text" required value={newTweet} onChange={onChange} />
+            <input type="submit" value="수정하기" />
+          </form>
+          <button onClick={toggleIsEditing}>취소</button>
+        </>
+      ) : (
+        <>
+          <h4>{tweet.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>삭제하기</button>
+              <button onClick={toggleIsEditing}>수정하기</button>
+            </>
+          )}
         </>
       )}
     </div>
