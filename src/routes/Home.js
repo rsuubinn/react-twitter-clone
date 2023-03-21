c2a18475d1d7b2b02d2ba5ee4a04346e84643b07
rsuubinn <rsuubinn@gmail.com>
2023-03-21 21:43:06 +0900

    feat : 이미지 업로드

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,45 @@
 import React, { useEffect, useState } from "react";
 import Tweet from "../components/Tweet";
-import { dbService } from "../firebase";
+import { dbService, storageService } from "../firebase";
+import { v4 as uuidv4 } from "uuid";
 
 const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]);
+  const [attachment, setAttachment] = useState();
   const onChange = (event) => {
     setTweet(event.target.value);
   };
   const onSubmit = async (event) => {
     event.preventDefault();
-    await dbService.collection("tweets").add({
-      text: tweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-    });
-    setTweet("");
+    const fileRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+    const response = await fileRef.putString(attachment, "data_url");
+    console.log(response);
+    // await dbService.collection("tweets").add({
+    //   text: tweet,
+    //   createdAt: Date.now(),
+    //   creatorId: userObj.uid,
+    // });
+    // setTweet("");
+  };
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const image = files[0];
+    const reader = new FileReader();
+    reader.onload = (finishEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(image);
+  };
+
+  const onClearAttachment = () => {
+    setAttachment(null);
   };
 
   useEffect(() => {
@@ -36,7 +60,14 @@ const Home = ({ userObj }) => {
           type="text"
           placeholder="What's on your mind?"
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="tweet" />
+        {attachment && (
+          <>
+            <img src={attachment} width="100px" height="100px" />
+            <button onClick={onClearAttachment}>지우기</button>
+          </>
+        )}
       </form>
       <div>
         {tweets.map((tweet) => (
